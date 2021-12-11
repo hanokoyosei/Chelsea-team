637deedfcf123d378ea728bd2c0d4a12835a9c4e
hanokoyosei <perizatonolbekova17@gmail.com>
2021-12-12 01:44:18 +0600

    index.js cleaned

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-<<<<<<< HEAD
-=======
-
->>>>>>> c98fd58e52c2e689a9fe349b2be7a44094204528
 
 ReactDOM.render(<App />,
   document.getElementById('root')
