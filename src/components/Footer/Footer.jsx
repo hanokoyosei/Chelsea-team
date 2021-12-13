433ae28a11a6cb3260b600b4123b6fd098e5f8be
Akjolbek <akybek01@gmail.com>
2021-12-13 11:09:54 +0600

    first commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,147 +1,85 @@
-import React, { useContext, useEffect } from "react";
-import { useNavigate, useParams } from "react-router";
-import { Button, Form, Input, Select, InputNumber } from "antd";
-import { productsContext } from "../../contexts/productsContext";
-import { brandsContext } from "../../contexts/brandsContext";
-
-const EditProduct = () => {
-  const params = useParams();
-  const navigate = useNavigate();
-  const { getOneProduct, oneProduct, updateProduct } =
-    useContext(productsContext);
-  const { getBrands, brands } = useContext(brandsContext);
-  const [form] = Form.useForm();
-  useEffect(() => {
-    getOneProduct(params.id);
-    getBrands();
-  }, []);
-  useEffect(() => {
-    form.setFieldsValue(oneProduct);
-  }, [oneProduct]);
-  const onFinish = (values) => {
-    console.log("Success:", values);
-    updateProduct(params.id, values).then(() => navigate("/admin"));
-  };
-  return (
-    <div className="container" style={{ marginTop: "15px" }}>
-      <h2>Edit product</h2>
-      <Form
-        name="basic"
-        onFinish={onFinish}
-        autoComplete="off"
-        layout="vertical"
-        form={form}
-      >
-        <Form.Item
-          label="Brand"
-          name="brand"
-          rules={[
-            {
-              required: true,
-              message: "Please input brand!",
-            },
-          ]}
-        >
-          <Select>
-            {brands.map((item) => (
-              <Select.Option key={item.id} value={item.brand}>
-                {item.brand}
-              </Select.Option>
-            ))}
-          </Select>
-        </Form.Item>
-
-        <Form.Item
-          label="Model"
-          name="model"
-          rules={[
-            {
-              required: true,
-              message: "Please input model!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Description"
-          name="description"
-          rules={[
-            {
-              required: true,
-              message: "Please input description!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Price"
-          name="price"
-          rules={[
-            {
-              required: true,
-              message: "Please input price!",
-            },
-          ]}
-        >
-          <InputNumber min={1} style={{ width: "100%" }} />
-        </Form.Item>
-
-        <Form.Item
-          label="Image 1"
-          name="image1"
-          rules={[
-            {
-              required: true,
-              message: "Please input URL of image 1!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Image 2"
-          name="image2"
-          rules={[
-            {
-              required: true,
-              message: "Please input URL of image 2!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Video"
-          name="video"
-          rules={[
-            {
-              required: true,
-              message: "Please input URL of video!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          wrapperCol={{
-            offset: 9,
-            span: 16,
-          }}
-        >
-          <Button type="primary" htmlType="submit">
-            Edit product
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
-  );
-};
-
-export default EditProduct;
+import React from "react"; 
+import "./Footer.css"; 
+import { 
+  InstagramOutlined, 
+  FacebookOutlined, 
+  TwitterOutlined, 
+  YoutubeOutlined, 
+  WhatsAppOutlined, 
+} from "@ant-design/icons"; 
+ 
+const Footer = () => { 
+  return ( 
+    <div className="first-footer"> 
+      <div className="back"> 
+        <a style={{ color: "#2554C7" }} onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}> 
+          BACK TO TOP 
+        </a> 
+      </div> 
+      <div className="footer"> 
+        <div className="foot"> 
+          <h3 style={{ color: "whitesmoke" }}>CUSTOMER CAR</h3> 
+          <a style={{ color: "white" }}>ContactUs</a> 
+          <a style={{ color: "white" }}>Luxury Services</a> 
+          <a style={{ color: "white" }}>Delivery Information</a> 
+          <a style={{ color: "white" }}>Click & Collect</a> 
+          <a style={{ color: "white" }}>Affirm Finance</a> 
+          <a style={{ color: "white" }}>WOS Credit Card</a> 
+          <a style={{ color: "white" }}>Return Policy</a> 
+          <a style={{ color: "white" }}>Frequently Asked Questions</a> 
+          <a style={{ color: "white" }}>Your Feedback</a> 
+        </div> 
+ 
+        <div className="foot"> 
+          <h3 style={{ color: "whitesmoke" }}>INFORMATION</h3> 
+          <a style={{ color: "whitesmoke" }}>Your Security</a> 
+          <a style={{ color: "whitesmoke" }}>Terms Of Service</a> 
+          <a style={{ color: "whitesmoke" }}>Privacy Policy</a> 
+          <a style={{ color: "whitesmoke" }}>Cookie Policy</a> 
+          <a style={{ color: "whitesmoke" }}>Accessibility</a> 
+          <a style={{ color: "whitesmoke" }}>Corporate Social Responsibility</a> 
+          <a style={{ color: "whitesmoke" }}>PayByLink Terms</a> 
+          <a style={{ color: "whitesmoke" }}>Calibre</a> 
+        </div> 
+ 
+        <div className="foot"> 
+          <h3 style={{ color: "whitesmoke" }}>ABOUT WOS US</h3> 
+          <a style={{ color: "whitesmoke" }}>About Us</a> 
+          <a style={{ color: "whitesmoke" }}>Press Room</a> 
+          <a style={{ color: "whitesmoke" }}>Careers</a> 
+          <a style={{ color: "whitesmoke" }}>Newsletter Sign Up</a> 
+        </div> 
+ 
+        <div className="foot"> 
+          <h3 style={{ color: "whitesmoke" }}>CATEGORIES</h3> 
+          <a style={{ color: "whitesmoke" }}>Watches</a> 
+          <a style={{ color: "whitesmoke" }}>Brands</a> 
+        </div> 
+ 
+        <div className="foot"> 
+          <h3 style={{ color: "whitesmoke" }}>FOLLOW US ON</h3> 
+          <div className="ins"> 
+            <a> 
+              <InstagramOutlined /> 
+            </a> 
+            <a> 
+              <FacebookOutlined /> 
+            </a> 
+            <a> 
+              <TwitterOutlined /> 
+            </a> 
+            <a> 
+              <YoutubeOutlined /> 
+            </a> 
+            <a> 
+              <WhatsAppOutlined /> 
+            </a> 
+          </div> 
+        </div> 
+      </div> 
+       
+    </div> 
+  ); 
+}; 
+ 
+export default Footer;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -65,18 +65,13 @@ const Home = () => {
           <Meta title="EFL" />
         </Card>
       </div>
-      <div style={{marginTop: '3%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+      {/* <div style={{marginTop: '3%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
       <a style={{color: 'black', fontWeight: 'bold'}}>ANYTIME ANYWHERE BY WATCHES OF SWITZERLAND</a>
       <p style={{marginTop: '1%'}}>A cinematic love letter to the modern collector</p>
       <Link to='/discover'><a style={{color: 'black', textDecoration: 'underline' }}>Discover Featured Watches</a></Link>
-      </div>
+      </div> */}
       <div>
       <Carousel autoplay>
-    <div>
-      <Link to="/img1">
-      <img src='https://fcwallpaper.com/wp-content/uploads/2020/05/Backgrounds-Chelsea-FC-HD.jpg' alt='' style={{width: '1489px', height: '700px'}} />
-      </Link>
-    </div>
     <div>
       <img src='https://i1.wp.com/www.thedabigal.com.ng/wp-content/uploads/2021/05/Chelsea-win-UCL-2021.jpg?fit=2048%2C1268&ssl=1' alt='' style={{width: '1489px', height: '700px'}} />
     </div>
@@ -88,13 +83,13 @@ const Home = () => {
     </div>
   </Carousel>,
       </div>
-      <div className="links" style={{marginTop: '3%', display: 'flex', flexDirection: 'row'}}>
+      {/* <div className="links" style={{marginTop: '3%', display: 'flex', flexDirection: 'row'}}>
         <a>
         <h3 style={{color: '#85754e'}}><img style={{width: '50px', height: '25px'}} src='https://content.thewosgroup.com/wosus/icons/affirm-logo-normal.png' alt='' style={{maxWidth: '64px', marginRight: '10px', verticalAlign: 'bottom'}} />FLEXIBLE FINANCING <br/> <p style={{color: 'black'}}>MAKE MONTHLY PAYMENTS WITH AFFIRM</p></h3>
         </a>
         <a><h3 style={{color: '#85754e'}}>AUTHORIZED LUXURY RETAILER<br/><p style={{color: 'black'}}>NOW AVAILABLE TO BUY ONLINE</p></h3></a>
         <a><h3  style={{color: '#85754e'}}>FREE HOME DELIVERY AVAILABLE<br/><p style={{color: 'black'}}>ESTIMATED DELIVERY 1-3 DAYS</p></h3></a>
-      </div>
+      </div> */}
       {/* <div style={{display: 'flex', justifyContent: 'center'}}>
         <img style={{width: '460px', height: '460px'}} src='https://www.watchesofswitzerland.com/medias/wosus-3wide-omega007-oct21.jpg?context=bWFzdGVyfHJvb3R8MzMwNDM5fGltYWdlL2pwZWd8aDEzL2hiYS85MTE5NjUxODIzNjQ2LmpwZ3xhMzdjMjI0OTJkMGY1OTRjYzRjNTIyZWI0ZTIxYmMzNzdhMzM1ZDE2M2RmMWJjNjExZWZjMTRkYzA4ZmY3NzNj&imwidth=640' alt='' />
         <img style={{width: '460px', height: '460px'}} src='https://www.watchesofswitzerland.com/medias/oris-sunwukong-wosushp-3wide-dec21.png?context=bWFzdGVyfHJvb3R8NTU5NDcyfGltYWdlL3BuZ3xoZDQvaGIxLzkxMjg5NDAxNDI2MjIucG5nfDMwOTEyYjI2MTJiMTg0YWI2MWI2ODVmNGQ1NDMwYjQyMzMwNzNkOTY0MTcyYjM4NWM1NjY5OTc3OWI4MzI5ZDM&imwidth=640' alt='' />
