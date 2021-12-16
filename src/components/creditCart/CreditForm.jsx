e42aad3a46b254948374b54fc159ffb65d29d1a3
Akjolbek <akybek01@gmail.com>
2021-12-16 15:41:30 +0600

    last

--- src/components/creditCart/CreditForm.jsx
+++ src/components/creditCart/CreditForm.jsx
@@ -1,4 +1,4 @@
-import { Form, Input, Select, Tooltip, Button, Space, Typography } from 'antd';
+import { Form, Input, Select, Button, Space } from 'antd';
 import { Link } from 'react-router-dom';
 import "./CreditForm.css"
 const { Option } = Select;
@@ -7,37 +7,27 @@ const creditForm = () => {
   const onFinish = values => {
     console.log('Received values of form: ', values);
   };
-    
-  
+     
   return (
       <div style={{marginTop:"50px"}}>
     <Form name="complex-form" onFinish={onFinish} labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
-      <Form.Item label="Username">
+      <Form.Item label={<h4 style={{color: 'white'}}>Username</h4>}>
         <Space>
           <Form.Item
             name="username"
             noStyle
             rules={[{ required: true, message: 'Username is required' }]}
           >
-            <Input style={{ width: 160 }} placeholder="Please input" />
+            <Input style={{ width: 350 }} placeholder="Please input" />
             <Form.Item
             name="username"
             noStyle
             rules={[{ required: true, message: 'Username is required' }]}
           />
-          
-         
-            
-          </Form.Item>
-          
-          <Tooltip title="Useful information">
-            <Typography.Link href="#API">нужна помощь?</Typography.Link>
-          </Tooltip>
-          
-        </Space>
-        
+          </Form.Item>          
+        </Space>        
       </Form.Item>
-      <Form.Item label="Address">
+      <Form.Item label={<h4 style={{color: 'white'}}>Adress</h4>}>
         <Input.Group compact>
           <Form.Item
             name={['address', 'province']}
@@ -49,29 +39,29 @@ const creditForm = () => {
               <Option value="Ош">Ош</Option>
             </Select>
           </Form.Item>
+          <br/>
+          <br/>
           <Form.Item
             name={['address', 'street']}
             noStyle
             rules={[{ required: true, message: 'Street is required' }]}
           >
-            <Input style={{ width: '50%' }} placeholder="Input street" />
+            <Input style={{ width: '300px' }} placeholder="Street" />
           </Form.Item>
         </Input.Group>
       </Form.Item>
-      <Form.Item label="BirthDate" style={{ marginBottom: 0 }}>
+      <Form.Item label={<h4 style={{color: 'white'}}>Birth Date</h4>} style={{ marginBottom: 0 }}>
         <Form.Item
           name="year"
           rules={[{ required: true }]}
-          style={{ display: 'inline-block', width: 'calc(50% - 8px)' }}
         >
-          <Input placeholder="Input birth year" />
+          <Input placeholder="Input birth year" style={{width: '300px'}} />
         </Form.Item>
         <Form.Item
           name="month"
           rules={[{ required: true }]}
-          style={{ display: 'inline-block', width: 'calc(50% - 8px)', margin: '0 8px' }}
         >
-          <Input placeholder="Input birth month" />
+          <Input placeholder="Input birth month" style={{width: '300px'}} />
         </Form.Item>
       </Form.Item>
       <Form.Item label=" " colon={false}>
